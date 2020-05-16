6d0e18c6bea940c3600cfc8d694b983d3d2a21fb
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2020-05-16 13:58:44 +0900

    Added version info for loader and library

--- sources/app_version.h
+++ sources/app_version.h
@@ -1,3 +1,9 @@
 #pragma once
-#define APP_VERSION_MAJOR 2
-#define APP_VERSION_MINOR 0
+#define MACRO_TO_STRING2(s)     #s
+#define MACRO_TO_STRING(s)      MACRO_TO_STRING2(s)
+
+#define APP_VERSION_MAJOR       2
+#define APP_VERSION_MINOR       1
+#define APP_VERSION_STRING      MACRO_TO_STRING(APP_VERSION_MAJOR)      \
+                                "." MACRO_TO_STRING(APP_VERSION_MINOR)  \
+                                "\0"
