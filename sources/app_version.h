0b4feadaf4d152e9357382a5f31aa408d9cab98c
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2020-06-27 18:24:25 +0900

    Updated app version header

--- sources/app_version.h
+++ sources/app_version.h
@@ -3,7 +3,7 @@
 #define MACRO_TO_STRING(s)      MACRO_TO_STRING2(s)
 
 #define APP_VERSION_MAJOR       2
-#define APP_VERSION_MINOR       1
+#define APP_VERSION_MINOR       2
 #define APP_VERSION_STRING      MACRO_TO_STRING(APP_VERSION_MAJOR)      \
                                 "." MACRO_TO_STRING(APP_VERSION_MINOR)  \
                                 "\0"
