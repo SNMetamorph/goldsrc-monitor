c58607d440588a9436e1ef6592bdffa347ad181f
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2020-07-22 00:30:43 +0900

    Added icon to loader program

---
+++ msvc/loader/resource.h
@@ -0,0 +1,19 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется loader.rc
+//
+#define APP_VERSION_MAJOR               2
+#define APP_VERSION_MINOR               3
+#define IDI_ICON1                       101
+#define IDI_ICON2                       102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
