dab9af4089f2577fb6fcc07c58b4a570df5df03b
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2020-08-11 22:24:53 +0400

    Resource files relocated to sources directory

--- sources/loader/resource.h
+++ sources/loader/resource.h
@@ -1,12 +1,17 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by loader.rc
+п»ї//{{NO_DEPENDENCIES}}
+// Р’РєР»СЋС‡Р°РµРјС‹Р№ С„Р°Р№Р», СЃРѕР·РґР°РЅРЅС‹Р№ РІ Microsoft Visual C++.
+// РСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ loader.rc
+//
+#define APP_VERSION_MAJOR               2
+#define APP_VERSION_MINOR               3
+#define IDI_ICON1                       101
+#define IDI_ICON2                       102
 
-// Следующие стандартные значения для новых объектов
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
