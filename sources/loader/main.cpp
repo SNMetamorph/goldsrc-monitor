21485c4b663670dd79bea48601c7d03d5bc76be3
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2020-08-14 22:12:07 +0400

    Library injection algorithm inproved

--- sources/loader/main.cpp
+++ sources/loader/main.cpp
@@ -2,6 +2,7 @@
 #include "exception.h"
 #include "app_version.h"
 #include <iostream>
+#include <string>
 #include <Windows.h>
 #include <shlwapi.h>
 using namespace std;
@@ -37,29 +38,82 @@ static void OpenGameProcess(HANDLE &processHandle)
         EXCEPT("unable to found game process, try to run game");
 }
 
+static bool FindLibraryPath(std::wstring &libPath)
+{
+    libPath.reserve(MAX_PATH);
+    GetFullPathNameW(
+        LIBRARY_NAME, 
+        libPath.capacity(), 
+        libPath.data(), 
+        NULL
+    );
+
+    if (PathFileExistsW(libPath.data()))
+        return true;
+    else
+        return false;
+}
+
+static wchar_t *WritePathString(HANDLE procHandle, const std::wstring &libPath)
+{
+    size_t writtenBytes;
+    wchar_t *pathRemoteAddr;
+
+    // allocating memory in game process for library path string
+    pathRemoteAddr = (wchar_t*)VirtualAllocEx(
+        procHandle,
+        NULL,
+        libPath.capacity(),
+        MEM_RESERVE | MEM_COMMIT,
+        PAGE_READWRITE
+    );
+    if (!pathRemoteAddr)
+        return nullptr;
+
+    // writing string to game process
+    writtenBytes = 0;
+    WriteProcessMemory(
+        procHandle, pathRemoteAddr,
+        libPath.data(), libPath.capacity(),
+        (SIZE_T*)&writtenBytes
+    );
+
+    if (libPath.capacity() != writtenBytes)
+        return nullptr;
+
+    return pathRemoteAddr;
+}
+
+static int GetFuncReturnCode(HANDLE threadHandle)
+{
+    DWORD exitCode;
+    GetExitCodeThread(threadHandle, &exitCode);
+    return exitCode;
+}
+
 static void InjectLibrary(HANDLE procHandle)
 {
-    HANDLE          threadID;
-    size_t			funcOffset;
-    size_t          writtenBytes;
-    void            *pathRemoteAddr;
+    std::wstring    libraryPath;
+    wchar_t         *pathStrRemote;
     HMODULE         k32LocalHandle;
     HMODULE			k32RemoteHandle;
     moduleinfo_t	k32Info;
-    static wchar_t  libPath[MAX_PATH];
-    const size_t    pathSize = sizeof(libPath);
-    LPTHREAD_START_ROUTINE funcRemoteAddr;
+    HANDLE          threadHandle;
+    size_t			funcOffset;
+    uint8_t         *funcRemote;
+  
+    if (!FindLibraryPath(libraryPath))
+        EXCEPT("library file not found, make sure that you unpacked program from archive");
 
-    GetFullPathName(LIBRARY_NAME, MAX_PATH, libPath, NULL);
-    if (!PathFileExists(libPath))
-        EXCEPT("library file not found");
+    pathStrRemote = WritePathString(procHandle, libraryPath);
+    if (!pathStrRemote)
+        EXCEPT("unable to write library path string in game process memory");
 
     /*
-        getting address of LoadLibrary() in game process
-        ----------------------------------------------
-        it's simple method to get address of function from remote process
-        and will work in most cases, if kernel32.dll from game process
-        isn't differ with same library from loader
+    getting address of LoadLibrary() in game process
+    it's simple method to get address of function in remote process
+    and it will work in most cases, if kernel32.dll from game process
+    isn't differ with same kernel32.dll from loader process
     */
     k32LocalHandle = GetModuleHandle(L"kernel32.dll");
     k32RemoteHandle = FindProcessModule(procHandle, L"kernel32.dll");
@@ -70,39 +124,28 @@ static void InjectLibrary(HANDLE procHandle)
     if (!GetModuleInfo(procHandle, k32RemoteHandle, k32Info))
         EXCEPT("GetModuleInfo() for remote kernel32.dll failed");
 
-    // allocating memory in game process for library path
-    pathRemoteAddr = VirtualAllocEx(
-        procHandle,
-        NULL,
-        pathSize,
-        MEM_RESERVE | MEM_COMMIT,
-        PAGE_READWRITE
-    );
-    if (!pathRemoteAddr)
-        EXCEPT("unable to allocate memory in game process");
-
-    // writing string to game process
-    writtenBytes = 0;
-    WriteProcessMemory(
-        procHandle, pathRemoteAddr,
-        libPath, pathSize,
-        (SIZE_T*)&writtenBytes
-    );
-
-    if (pathSize != writtenBytes)
-        EXCEPT("unable to write library path");
-
     // creating thread in game process and invoking LoadLibrary()
+    cout << "Starting injection thread in remote process..." << endl;
     funcOffset = GetFunctionOffset(k32LocalHandle, "LoadLibraryW");
-    funcRemoteAddr = (LPTHREAD_START_ROUTINE)(k32Info.baseAddr + funcOffset);
-    threadID = CreateRemoteThread(procHandle,
+    funcRemote = k32Info.baseAddr + funcOffset;
+    threadHandle = CreateRemoteThread(procHandle,
         0, 0,
-        funcRemoteAddr, pathRemoteAddr,
+        (LPTHREAD_START_ROUTINE)funcRemote, pathStrRemote,
         0, 0
     );
 
-    if (!threadID)
+    if (!threadHandle)
         EXCEPT("unable to create remote thread");
+
+    // wait for thread in game process exits
+    if (WaitForSingleObject(threadHandle, 6000) == WAIT_TIMEOUT)
+        EXCEPT("library injection thread timed out");
+
+    int exitCode = GetFuncReturnCode(threadHandle);
+    if (exitCode)
+        cout << "Injected library base address: 0x" << hex << exitCode << endl;
+    else
+        EXCEPT("remote thread failed to load library");
 }
 
 static void PrintTitleText()
@@ -134,14 +177,13 @@ int main(int argc, char *argv[])
             if (!FindProcessModule(gameProcess, LIBRARY_NAME))
             {
                 InjectLibrary(gameProcess);
-                Sleep(300);
                 if (FindProcessModule(gameProcess, LIBRARY_NAME))
                 {
                     cout << "Library successfully injected: check game console for more info" << endl;
                     break;
                 }
                 else
-                    EXCEPT("library injection performed, but nothing changed");
+                    EXCEPT("library injection performed, but module not found");
             }
             else
             {
