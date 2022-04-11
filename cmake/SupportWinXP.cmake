48fa97873017178b13a148101f8e5c750f9fd163
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-04-11 18:48:55 +0400

    Updated and refactored CMake scripts

--- cmake/SupportWinXP.cmake
+++ cmake/SupportWinXP.cmake
@@ -5,20 +5,30 @@ set (WINXPSUPPORT_INCLUDED true)
 
 #!	Applies some variables and compiler flags, 
 #	which provides legacy Windows XP support.
-#	Affects targets from current and below directories.
+#
+#	\arg:TARGET_NAME Name of the target, that need to be modified
+#
 
-macro (enable_winxp_support)
+macro (enable_winxp_support TARGET_NAME)
+	if (NOT TARGET ${TARGET_NAME})
+		message (FATAL_ERROR "enable_winxp_support(): target ${target_name} does not exist")
+	endif()
+	
 	if (WIN32)
 		if (MSVC)
 			if ("${CMAKE_GENERATOR}" MATCHES "Visual Studio ([0-9]+)")
-				add_definitions (/D_USING_V110_SDK71_)
+				target_compile_options (${TARGET_NAME} PRIVATE /D_USING_V110_SDK71_)
+				target_compile_options (${TARGET_NAME} PRIVATE /Zi)
+				target_compile_options (${TARGET_NAME} PRIVATE /FS)
+				target_compile_options (${TARGET_NAME} PRIVATE /Zc:threadSafeInit-)
+				#target_link_options (${TARGET_NAME} PRIVATE /SUBSYSTEM:CONSOLE,5.01)
 
 				if (${CMAKE_MATCH_1} LESS 11)
 					# Nothing. Older VS does support XP by default.
 				elseif (${CMAKE_MATCH_1} EQUAL 11)
 					# Visual Studio 11 2012
 					set (CMAKE_GENERATOR_TOOLSET "v110_xp" CACHE STRING "CMAKE_GENERATOR_TOOLSET" FORCE)
-					set (CMAKE_VS_PLATFORM_TOOLSET "v110_xp" CACHE STRING "CMAKE_VS_PLATFORM_TOOLSET" FORCE)
+					set (CMAK_VS_PLATFORM_TOOLSET "v110_xp" CACHE STRING "CMAKE_VS_PLATFORM_TOOLSET" FORCE)
 				elseif (${CMAKE_MATCH_1} EQUAL 12)
 					# Visual Studio 12 2013
 					set (CMAKE_GENERATOR_TOOLSET "v120_xp" CACHE STRING "CMAKE_GENERATOR_TOOLSET" FORCE)
@@ -35,16 +45,16 @@ macro (enable_winxp_support)
 					# Visual Studio 16 2019
 					set (CMAKE_GENERATOR_TOOLSET "v141_xp" CACHE STRING "CMAKE_GENERATOR_TOOLSET" FORCE)
 					set (CMAKE_VS_PLATFORM_TOOLSET "v141_xp" CACHE STRING "CMAKE_VS_PLATFORM_TOOLSET" FORCE)
-				else()
+				else ()
 					message (WARNING "enable_winxp_support(): unknown toolset number, skipping")
-				endif()
-			else()
+				endif ()
+			else ()
 				message (FATAL_ERROR "enable_winxp_support(): CMake generator doesn't match Visual Studio")
-			endif()	
-		else()
+			endif ()	
+		else ()
 			message (WARNING "enable_winxp_support(): compiler is not MSVC, skipping")
-		endif()
-	else()
+		endif ()
+	else ()
 		message (WARNING "enable_winxp_support(): target OS is not Windows, skipping")
-	endif()
-endmacro()
+	endif ()
+endmacro ()
