c55b22a326d01dc4672dfa3e7f5f37547630511d
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-08-07 21:31:26 +0400

    Updated version number

--- sources/app_info.h
+++ sources/app_info.h
@@ -8,8 +8,8 @@
 #define APP_TITLE_STR           "GoldSrc Monitor"
 #define APP_GITHUB_LINK         "https://github.com/SNMetamorph/goldsrc-monitor"
 #define APP_BUILD_DATE          (__DATE__ " " __TIME__)
-#define APP_VERSION_MAJOR       2
-#define APP_VERSION_MINOR       8
+#define APP_VERSION_MAJOR       3
+#define APP_VERSION_MINOR       0
 #define APP_VERSION_STRING      MACRO_TO_STRING(APP_VERSION_MAJOR)      \
                                 "." MACRO_TO_STRING(APP_VERSION_MINOR)  \
                                 "\0"
